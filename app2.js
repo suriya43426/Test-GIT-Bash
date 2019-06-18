e07169e972bc05cff2a9a29c4081ec1cbcd5ade6
suriya43426 <suriya43426@hotmail.com>
2019-06-18 11:56:53 +0700

    selection Option 2Way

--- app2.js
+++ app2.js
@@ -3,30 +3,12 @@ new Vue({
     data:{
         name:"Suriya C",
         job:"engineer",
-        age:0,
-        count:0,
-        status:false,
-        website: 'http://www.ksl-calc.com'
+        website: 'http://www.ksl-calc.com',
+        foods:[],
+        gender:[],
+        job:[]
     },
     methods :{
-        getName:function(){
-            return this.name
-        },
-        setName:function(n){
-            return this.name=n
-        },
-        addAge:function(){
-            this.age++;
-            console.log(this.age);
-        },
-        SubtractAge:function(){
-            this.age--;
-            console.log(this.age);
-        },
-        addCount:function(){
-            this.count++;
-            console.log(this.count);
-        }
-
+       
     }
 })
