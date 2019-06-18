d763ee51f29cc013b717c8c5596c6e7cf55851a2
suriya43426 <suriya43426@hotmail.com>
2019-06-18 17:18:12 +0700

    15

--- app.js
+++ app.js
@@ -1,50 +1,26 @@
-new Vue({
-    el:'#vue-app',
+var post=new Vue({
+    el:"#post",
     data:{
-        newData:{
-            empName:'',
-            salary:0
-        },
-        empGroup:[
-        {
-            empName:"สมพร",
-            salary:5000
-        },
-        {
-            empName:"สมศรี",
-            salary:4000
-        }
-        ]
-    },methods:{
-        addEmp:function(){
-            this.empGroup.push({
-                empName: this.newData.empName,
-                salary: this.newData.salary
-            });
-            this.newData.empName = '';
-            this.newData.salary = 0;
-        },showMessage:function(){
-            console.log("Send data");
-        }
+        title:'ส่งโปรเจค',
+        message:'รายละเอียดโปรเจค'
+    }
+});
+
+var comment=new Vue({
+    el:"#comment",
+    data:{
+        text:'อากาศดีมาก'
+    }
+});
+
+var likeSystem=new Vue({
+    el:"#likeSystem",
+    data:{
+        like:true
     },
-    computed:{
-        summation:function(){
-            var sum = this.empGroup.reduce(function(value, data) {
-            return value + Number(data.salary);
-            },0);
-            return sum;
-        },
-        avg:function(){
-            var sum = this.empGroup.reduce(function(value, data) {
-            return value + Number(data.salary);
-            },0);
-            return sum/this.empGroup.length;
-        }
-    }, watch: {
-        //หลังจากที่ avg ทำงานเสร็จ
-        //ให้ไปใช้งานคำสั่ง showMessage
-        summation: function(){
-            this.showMessage();
+    methods:{
+        changeLike:function(){
+            console.log("like");
         }
     }
 });
