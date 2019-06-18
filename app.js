c094f7c1b3aa893e186b11a876ed58f042d79f76
suriya43426 <suriya43426@hotmail.com>
2019-06-18 16:13:32 +0700

    14 Watcher DB

--- app.js
+++ app.js
@@ -23,7 +23,28 @@ new Vue({
             });
             this.newData.empName = '';
             this.newData.salary = 0;
-
+        },showMessage:function(){
+            console.log("Send data");
+        }
+    },
+    computed:{
+        summation:function(){
+            var sum = this.empGroup.reduce(function(value, data) {
+            return value + Number(data.salary);
+            },0);
+            return sum;
+        },
+        avg:function(){
+            var sum = this.empGroup.reduce(function(value, data) {
+            return value + Number(data.salary);
+            },0);
+            return sum/this.empGroup.length;
+        }
+    }, watch: {
+        //หลังจากที่ avg ทำงานเสร็จ
+        //ให้ไปใช้งานคำสั่ง showMessage
+        summation: function(){
+            this.showMessage();
         }
     }
 });
