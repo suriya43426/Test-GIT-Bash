c48ac61b527ee62a707201eb2ac9e3e1627f728b
suriya43426 <suriya43426@hotmail.com>
2019-06-18 14:13:42 +0700

    11 loop v for

--- app2.js
+++ app2.js
@@ -1,7 +1,12 @@
 new Vue({
     el:'#vue-app2',
     data:{
-       like:false
+       like:false,
+       product:['ผงซักฟอก','แปลงสีฟัน','สบู่','ผ้าขนหนู','กางเกง','เสื้อ'],
+       employee:[
+           {name:'พงษ์ศักดิ์',age:25,status:false},
+           {name:'สมพงษ์',age:30,status:true}
+       ]
     },
     methods :{
        
