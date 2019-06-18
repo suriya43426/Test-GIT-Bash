dd0d575eea098e410dd81ff3cc0edcedcc6309e7
suriya43426 <suriya43426@hotmail.com>
2019-06-18 12:14:10 +0700

    Like

--- app2.js
+++ app2.js
@@ -1,12 +1,7 @@
 new Vue({
     el:'#vue-app2',
     data:{
-        name:"Suriya C",
-        job:"engineer",
-        website: 'http://www.ksl-calc.com',
-        foods:[],
-        gender:[],
-        job:[]
+       like:false
     },
     methods :{
        
