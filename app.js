a8d39843dbb6fa3263b9f3eb75df8e12dae3b700
suriya43426 <suriya43426@hotmail.com>
2019-06-18 14:21:16 +0700

    12 method

---
+++ app.js
@@ -0,0 +1,19 @@
+new Vue({
+    el:'#vue-app',
+    data:{
+        newData:{
+            empName:'',
+            salary:0
+        },
+        empGroup:[
+        {
+            empName:"สมพร",
+            salary:5000
+        },
+        {
+            empName:"สมศรี",
+            salary:4000
+        },
+        ]
+    }
+})
