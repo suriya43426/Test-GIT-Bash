7db2c9477c673f6677b0964489446c917b0b0f68
suriya43426 <suriya43426@hotmail.com>
2019-06-18 17:33:17 +0700

    16 component

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
-var post=new Vue({
-    el:"#post",
-    data:{
-        title:'ส่งโปรเจค',
-        message:'รายละเอียดโปรเจค'
-    }
-});
-
-var comment=new Vue({
-    el:"#comment",
-    data:{
-        text:'อากาศดีมาก'
-    }
-});
-
-var likeSystem=new Vue({
-    el:"#likeSystem",
-    data:{
-        like:true
-    },
-    methods:{
-        changeLike:function(){
-            console.log("like");
+// Custom Tag
+Vue.component('ohm',{
+    data:function(){
+        return{
+            count:0
         }
-    }
+    },
+    template:'<button v-on:click="count++">Click :{{count}}</button>'
+});
+
+Vue.component('post', {
+    template:'<h3>New post</h3>'
+})
+new Vue({
+    el:"#vue-app"
 });
