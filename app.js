c36595e07b2ee49900cfea2fc57a081eca4de070
suriya43426 <suriya43426@hotmail.com>
2019-06-19 09:34:26 +0700

    18 props validate

--- app.js
+++ app.js
@@ -12,6 +12,21 @@ Vue.component('post', {
     props:['title'],
     template:'<h3>{{title}}</h3>'
 })
+
+Vue.component('showview', {
+    props: {
+        title:{
+            type:String,
+            required:true
+        },
+        view:{
+            type:Number,
+            default:0
+        }
+    },
+    template:'<h3>{{title}} | {{view}}</h3>'
+})
+
 new Vue({
     el:"#vue-app"
 });
