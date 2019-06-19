b85a53b3cc7e524cadd9ffb9eb5640df032c9d5a
suriya43426 <suriya43426@hotmail.com>
2019-06-19 11:20:40 +0700

    20

--- app.js
+++ app.js
@@ -1,51 +1,9 @@
-// Custom Tag
-Vue.component('ohm',{
-    data:function(){
-        return{
-            count:0
-        }
-    },
-    template:'<button v-on:click="count++">Click :{{count}}</button>'
-});
-
-Vue.component('post', {
-    props:['title'],
-    template:'<h3>{{title}}</h3>'
-});
-
-Vue.component('showview', {
-    props: {
-        title:{
-            type:String,
-            required:true
-        },
-        view:{
-            type:Number,
-            default:0
-        }
-    },
-    template:'<h3>{{title}} | {{view}}</h3>'
-});
-
-Vue.component('showcomment',{
-    //ตัวหนา เอียง
-    //การแสดงผ่านตัว props
-    props:['commentpost'],
-    template:'<li><i>{{commentpost}}</i><li>'    
-});
-
+var food={
+    template:'#foodarea'
+};
 new Vue({
     el:"#vue-app",
-    data:{
-        newComment:'',
-        comments:[
-            'สวัสดีตอนเช้า','ทักทาย','ยินดีด้วย','สบายดี','กำลังนอน'
-        ]
-    },
-    methods: {
-        addComment:function(){
-            this.comments.push(this.newComment)
-            this.newComment=''
-        }
-    },
-});
+    components:{
+        foodApp:food
+    }
+})
